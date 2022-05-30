95c13c4ff59b4ca55c377ec84dfd16d9b24390b9
ofantomas <tsypinartem9@gmail.com>
2022-05-31 01:05:52 +0300

    Remove std projection

--- tqc/actor_critic.py
+++ tqc/actor_critic.py
@@ -24,8 +24,8 @@ class Actor(nn.Module):
         output_modules = [ 
                                 spk.atomistic.Atomwise(
                                     n_in=schnet.n_atom_basis,
-                                    n_out=out_embedding_size * 4,
-                                    n_neurons=[out_embedding_size * 2],
+                                    n_out=out_embedding_size * 2 + 3,
+                                    n_neurons=[out_embedding_size],
                                     contributions='kv'
                                 )
                             ]
@@ -33,29 +33,28 @@ class Actor(nn.Module):
     
     def forward(self, state_dict):
         kv = self.model(state_dict)['kv']
-        k_mu, v_mu, k_sigma, v_sigma = torch.split(kv, self.out_embedding_size, dim=-1)
+        k_mu, v_mu, actions_log_std = torch.split(kv, [self.out_embedding_size, self.out_embedding_size, 3], dim=-1)
         # Calculate mean and std of shifts relative to other atoms
         rel_shifts_mean = torch.matmul(k_mu, v_mu.transpose(1, 2))
-        rel_shifts_log_std = torch.matmul(k_sigma, v_sigma.transpose(1, 2))
 
         # Calculate matrix of 1-vectors to other atoms
         P = state_dict['_positions'][:, :, None, :] - state_dict['_positions'][:, None, :, :]
         norm = torch.norm(P, p=2, dim=-1) + 1e-8
         P /= norm[..., None]
+        # Project actions
+        actions_mean = (P * rel_shifts_mean[..., None]).sum(-2)
 
         if self.training:
-            # Calculate mean and std of actions
-            actions_mean = (P * rel_shifts_mean[..., None]).sum(-2)
-            actions_log_std = (P * rel_shifts_log_std[..., None]).sum(-2)
+            # Clamp and exp log_std
             actions_log_std = actions_log_std.clamp(*LOG_STD_MIN_MAX)
             actions_std = torch.exp(actions_log_std)
             # Sample bounded actions and calculate log prob
-            tanh_normal = TanhNormal(actions_mean, actions_std, self.action_scale)
+            tanh_normal = TanhNormal(actions_mean, actions_std)
             actions, pre_tanh = tanh_normal.rsample()
             log_prob = tanh_normal.log_prob(pre_tanh)
             log_prob = log_prob.sum(dim=(1, 2)).unsqueeze(-1)
         else:
-            actions = self.action_scale * torch.tanh((P * rel_shifts_mean[..., None]).sum(-2))
+            actions = torch.tanh(actions_mean)
             log_prob = None
 
         return actions, log_prob
@@ -106,21 +105,19 @@ class Critic(nn.Module):
 class TanhNormal(Distribution):
     arg_constraints = {}
 
-    def __init__(self, normal_mean, normal_std, tanh_scale=1.0):
+    def __init__(self, normal_mean, normal_std):
         super().__init__()
         self.normal_mean = normal_mean
-        self.tanh_scale = torch.FloatTensor([tanh_scale]).to(DEVICE)
         self.normal_std = normal_std
         self.standard_normal = Normal(torch.zeros_like(self.normal_mean, device=DEVICE),
                                       torch.ones_like(self.normal_std, device=DEVICE))
         self.normal = Normal(normal_mean, normal_std)
 
     def log_prob(self, pre_tanh):
-        log_det = 2 * np.log(2) + F.logsigmoid(2 * pre_tanh) + F.logsigmoid(-2 * pre_tanh) +\
-                  np.prod(self.normal_mean.shape[1:]).item() * torch.log(self.tanh_scale)
+        log_det = 2 * np.log(2) + F.logsigmoid(2 * pre_tanh) + F.logsigmoid(-2 * pre_tanh)
         result = self.normal.log_prob(pre_tanh) - log_det
         return result
 
     def rsample(self):
         pretanh = self.normal_mean + self.normal_std * self.standard_normal.sample()
-        return self.tanh_scale * torch.tanh(pretanh), pretanh
+        return torch.tanh(pretanh), pretanh
