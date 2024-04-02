1095db58cdc9159269f583c862d6a840c03e99f2
ofantomas <tsypinartem9@gmail.com>
2024-04-02 14:12:16 +0300

    Update training scripts

--- scripts/training/run_training_GOLF_10k.sh
+++ scripts/training/run_training_GOLF_10k.sh
@@ -4,25 +4,27 @@ cuda=$1
 
 CUDA_VISIBLE_DEVICES=$cuda \
 python ../../main.py --n_parallel 36 \
---n_threads 18 \
---db_path ../../data/GOLF_train.db \
---eval_db_path ../../data/GOLF_test.db \
+--n_threads 12 \
+--db_path ../../data/D-0.db \
+--eval_db_path ../../data/D-test.db \
 --num_initial_conformations -1 \
+--eval_num_initial_conformations 2000 \
 --sample_initial_conformations True \
 --timelimit_train 100 \
 --timelimit_eval 50 \
 --terminate_on_negative_reward True \
 --max_num_negative_rewards 1 \
 --reward dft \
+--surrogate_oracle_type rdkit \
+--tau 1.0 \
 --minimize_on_every_step True \
 --nnp_type DimenetPlusPlus \
---nnp_config_path ../../configs/dimenetplusplus.yaml \
+--nnp_config_path /mnt/2tb/tsypin/MARL/GOLF-pyg/configs/dimenetplusplus.yaml \
 --actor GOLF \
 --experience_saver reward_threshold \
 --store_only_initial_conformations False \
 --conformation_optimizer LBFGS \
 --conf_opt_lr 1.0 \
---conf_opt_lr_scheduler Constant \
 --max_iter 5 \
 --lbfgs_device cpu \
 --momentum 0.0 \
@@ -41,13 +43,14 @@ python ../../main.py --n_parallel 36 \
 --utd_ratio 50 \
 --subtract_atomization_energy True \
 --forces_norm_limit 1.0 \
---eval_freq 180 \
+--eval_freq 360 \
 --n_eval_runs 36 \
 --eval_termination_mode fixed_length \
---exp_name GOLF-10k \
+--exp_name GOLF-10k-Dimenet \
 --host_file_path ../../env/host_names.txt \
 --full_checkpoint_freq  720 \
 --light_checkpoint_freq 1440 \
 --save_checkpoints True \
---load_baseline ../../checkpoints/DimenetPlusPlus-best/NNP_checkpoint \
+--load_baseline ../../checkpoints/DimenetPlusPlus/NNP_checkpoint \
 --log_dir ../../results \
+--project_name GOLF-pyg \
