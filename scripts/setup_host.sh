79992dd53f67a7abef8e11e3b39127cc3d0b2e87
ofantomas <tsypinartem9@gmail.com>
2024-05-11 16:00:08 +0300

    Update setup script

--- scripts/setup_host.sh
+++ scripts/setup_host.sh
@@ -1,11 +1,12 @@
 #! /bin/bash -ex
 
-n_ports=$1
-begin_range_train=$2
+num_threads=$1
+n_ports=$2
+begin_range_train=$3
 ./install_miniconda.sh
 source ~/.bashrc
 source ~/miniconda3/etc/profile.d/conda.sh
 ./install_env.sh
 conda activate golf_dft_env
-./babysit_dft.sh $n_ports $begin_range_train
+./babysit_dft.sh $num_threads $n_ports $begin_range_train
 
